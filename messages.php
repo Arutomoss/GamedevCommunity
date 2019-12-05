855ab6286d66cc210d93e028a7a4e07e4aebb7a5
Arutomu <43913712+Arutomoss@users.noreply.github.com>
2019-12-05 12:32:06 +0300

    putting header css styles into a separate file

--- messages.php
+++ messages.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style-messages.css">
     <title>Messages</title>
 </head>
@@ -13,12 +17,20 @@
     <?php require 'blocks/headder.php'; ?>
 
     <div class="container">
-        <div class="wrapper">
-            <div class="chats">
-                
+        <div class="row justify-content-md-center">
+            <div class="chats col-3 mr-3">
+                <div class="row">
+                    <img src="img/search.svg" alt="">
+                    <input type="text" style="border-radius: 25px; border: none; background-color: #303030; color: #7B7B7B;" placeholder="Поиск" >
+                </div>
             </div>
-            <div class="messages">
+            <div class="messages row col-7 ml-1">
+                <div class="row">
 
+                </div>
+                <div class="row stretch" style="height: 500px; background-color: #252525">
+
+                </div>
             </div>
         </div>
     </div>
