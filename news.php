dc58a5ba5c4a50cee3b6a3541b2026bd20c31e40
Arutomoss <Arutomoss@users.noreply.github.com>
2019-12-09 17:27:03 +0300

    minor

--- news.php
+++ news.php
@@ -14,12 +14,12 @@
 
 <body>
     <?php
-        // if ($_COOKIE['user'] == ''){
-        //     // header('Location: sign_in.php');
-        // }
-        require 'blocks/headder.php';
-    ?> 
-   
+    // if ($_COOKIE['user'] == ''){
+    //     // header('Location: sign_in.php');
+    // }
+    require 'blocks/headder.php';
+    ?>
+
 
     <div class="m_container">
         <div class="main">
@@ -74,8 +74,89 @@
                     </div>
                 </div>
             </div>
+
+            <div class="content">
+                <div class="content-icon">
+                    <img src="img/logo_light.svg" alt="" height="45px">
+                </div>
+                <div class="wrap">
+                    <div class="content-headder">
+                        <div class="content-headder-title">
+                            <p>Insomnia Game News</p>
+                            <!-- <div class="title-info">
+                                <p>@IGN </p>
+                                <p>• 20 мин</p>
+                            </div> -->
+                        </div>
+                        <div class="content-discription">
+                            <p>Sony говорит, что PS5 сможет использовать 8K графику ... но мы не ожидаем, что она сможет
+                                достичь всего этого разрешения изначально. Вот как их консоль следующего поколения
+                                сможет
+                                реализовать все эти пиксели в 2020 году.</p>
+                        </div>
+                    </div>
+                    <div class="content-source">
+                        <img src="img/post_image_1.jpg" alt="">
+                    </div>
+                    <div class="content-bottom-panel">
+                        <div class="content-bottom-panel-comments">
+                            <img src="img/comments.svg" alt="">
+                            <p>34</p>
+                        </div>
+                        <div class="content-bottom-panel-reposts">
+                            <img src="img/reposts.svg" alt="">
+                            <p>9</p>
+                        </div>
+                        <div class="content-bottom-panel-likes">
+                            <img src="img/likes.svg" alt="">
+                            <p>734</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="content">
+                <div class="content-icon">
+                    <img src="img/logo_light.svg" alt="" height="45px">
+                </div>
+                <div class="wrap">
+                    <div class="content-headder">
+                        <div class="content-headder-title">
+                            <p>Insomnia Game News</p>
+                            <!-- <div class="title-info">
+                                <p>@IGN </p>
+                                <p>• 20 мин</p>
+                            </div> -->
+                        </div>
+                        <div class="content-discription">
+                            <p>Sony говорит, что PS5 сможет использовать 8K графику ... но мы не ожидаем, что она сможет
+                                достичь всего этого разрешения изначально. Вот как их консоль следующего поколения
+                                сможет
+                                реализовать все эти пиксели в 2020 году.</p>
+                        </div>
+                    </div>
+                    <div class="content-source">
+                        <img src="img/post_image_1.jpg" alt="">
+                    </div>
+                    <div class="content-bottom-panel">
+                        <div class="content-bottom-panel-comments">
+                            <img src="img/comments.svg" alt="">
+                            <p>34</p>
+                        </div>
+                        <div class="content-bottom-panel-reposts">
+                            <img src="img/reposts.svg" alt="">
+                            <p>9</p>
+                        </div>
+                        <div class="content-bottom-panel-likes">
+                            <img src="img/likes.svg" alt="">
+                            <p>734</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        
+
         <div class="actual">
             <div class="actual-panel">
                 <div class="actual-panel-title">
