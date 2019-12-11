a6986485b592b895b153bbe19aa592e940def75d
Arutomoss <Arutomoss@users.noreply.github.com>
2019-12-11 18:20:32 +0300

    Adding jams creation page

--- jam_creation.php
+++ jam_creation.php
@@ -7,14 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=cyrillic,cyrillic-ext,latin-ext,vietnamese" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style-jams.css">
+    <link rel="stylesheet" href="css/style-jam_creation.css">
     <title>Jams</title>
 </head>
 
 <body>
     <?php require 'blocks/headder.php'; ?>
 
+    <div class="mx-auto col-3 settings-panel">
+
+    </div>
+    <div class="col-5 jam">
     
+    </div>
 
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/popper.min.js"></script>
