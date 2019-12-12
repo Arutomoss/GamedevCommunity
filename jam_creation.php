ae4bbf369e288feffd0d1589c16ccae213b9b352
Arutomoss <Arutomoss@users.noreply.github.com>
2019-12-12 13:28:23 +0300

    minor

--- jam_creation.php
+++ jam_creation.php
@@ -16,9 +16,15 @@
 
     <div class="container ">
         <div class="row">
-            <div class="col-3 mr-3 settings-panel">
-                <div class="row">
-                    
+            <div class="col-3 mr-3 settings-panel" >
+                <div class="row" style="padding-left: 30px; padding-right: 30px;">
+                    <p style=" margin-top: 35px; color: #E8E8E8; font-size: 16px; font-family: montserrat; border-bottom: 2px solid #404040; width: 100%; padding-bottom: 5px;">Цвета</p>
+                </div>
+                <div class="row justify-content-between" style="padding-left: 30px; padding-right: 30px; color: #E8E8E8">
+                    Цвет текста
+                    <input type="text" class="rounded-25 border-0" placeholder="#FFFFFF" style="margin-bottom: 12px; width: 110px;"><div class="circle" style="height: 18px; width: 18px; margin-top: 3px; margin-left: 218px; border-radius: 50%; background-color: #E8E8E8; position: fixed;"></div>
+                    Цвет фона
+                    <input type="text" class="rounded-25 border-0" placeholder="#303030" style="margin-bottom: 12px; width: 110px;"><div class="circle" style="height: 18px; width: 18px; margin-top: 39px; margin-left: 218px; border-radius: 50%; background-color: #303030; position: fixed;"></div>
                 </div>
             </div>
             <div class="col-8 jam-place">
@@ -34,15 +40,15 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="form-group col-12 jam-description">
-                                <textarea class="form-control border-0" id="exampleFormControlTextarea1" rows="5" style="background-color: #303030; color: #F1F1F1; font-family: montserrat; font-size: 14px; padding-left: 0px; padding-right: 0px;" placeholder="Введите описание..."></textarea>
+                            <div class="form-group col-12">
+                                <textarea class="form-control border-0" id="exampleFormControlTextarea1" rows="1" style="background-color: #303030; color: #F1F1F1; font-family: montserrat; font-size: 13px; padding-left: 0px; padding-right: 0px; margin-top: -10px;" placeholder="Введите краткое описание..."></textarea>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="form-group col-12">
-                                <textarea class="form-control border-0" id="exampleFormControlTextarea1" rows="1" style="background-color: #303030; color: #F1F1F1; font-family: montserrat; font-size: 14px; padding-left: 0px; padding-right: 0px;" placeholder="Введите краткое описание..."></textarea>
+                            <div class="form-group col-12 jam-description">
+                                <textarea class="form-control border-0" id="exampleFormControlTextarea1" rows="5" style="background-color: #303030; color: #F1F1F1; font-family: montserrat; font-size: 14px; padding-left: 0px; padding-right: 0px;" placeholder="Введите описание..."></textarea>
                             </div>
-                        </div>
+                        </div>                        
                     </div>
                 </form>
 
